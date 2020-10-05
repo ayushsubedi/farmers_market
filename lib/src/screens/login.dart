1976cfee3d9251d7701a93a11f215f5e608a6014
Ayush Subedi <ayush.subedi@gmail.com>
2020-10-05 20:43:00 +0545

    add cupertino style design

--- lib/src/screens/login.dart
+++ lib/src/screens/login.dart
@@ -1,3 +1,6 @@
+import 'package:farmers_market/src/screens/base.dart';
+import 'package:farmers_market/src/styles/colors.dart';
+import 'package:farmers_market/src/styles/textfields.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'dart:io';
@@ -30,8 +33,40 @@ class Login extends StatelessWidget {
       Container(
           height: 200,
           decoration: BoxDecoration(
-              image:
-                  DecorationImage(image: AssetImage('assets/images/logo.png'))))
+              image: DecorationImage(
+                  image: AssetImage('assets/images/logo.png')))),
+      Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: email(),
+      ),
+      Padding(
+        padding: EdgeInsets.symmetric(
+            horizontal: TextFieldStyles.textBoxHorizontal(),
+            vertical: TextFieldStyles.textBoxVertical()),
+        child: password(),
+      ),
     ]);
+  }
+
+  Widget email() {
+    if (Platform.isIOS) {
+      return CupertinoTextField(
+          padding: EdgeInsets.all(12.0),
+          placeholder: 'Email',
+          textAlign: TextAlign.center,
+          placeholderStyle: TextFieldStyles.placeholder(),
+          style: TextFieldStyles.text(),
+          cursorColor: TextFieldStyles.cursorColor(),
+          prefix: TextFieldStyles.iconPrefix(CupertinoIcons.mail_solid),
+          decoration: TextFieldStyles.cupertinoDecoration());
+    }
+    return TextField();
+  }
+
+  Widget password() {
+    if (Platform.isIOS) {
+      return CupertinoTextField();
+    }
+    return TextField();
   }
 }
---
+++ lib/src/styles/textfields.dart
@@ -0,0 +1,48 @@
+import 'package:farmers_market/src/screens/base.dart';
+import 'package:farmers_market/src/styles/colors.dart';
+import 'package:farmers_market/src/styles/text.dart';
+import 'package:flutter/material.dart';
+
+abstract class TextFieldStyles {
+  static double textBoxHorizontal() {
+    return 25.0;
+  }
+
+  static double textBoxVertical() {
+    return 8.0;
+  }
+
+  static TextStyle text() {
+    return TextStyles.body();
+  }
+
+  static TextStyle placeholder() {
+    return TextStyles.suggestion();
+  }
+
+  static Color cursorColor() {
+    return AppColors.darkblue();
+  }
+
+  static Widget iconPrefix(IconData icon) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 8.0),
+      child: Icon(
+        icon,
+        size: 35.0,
+        color: AppColors.lightblue(),
+      ),
+    );
+  }
+
+  static TextAlign textAlign() {
+    return TextAlign.center;
+  }
+
+  static BoxDecoration cupertinoDecoration() {
+    return BoxDecoration(
+        border: Border.all(
+            color: AppColors.straw(), width: BaseStyles.borderWidth()),
+        borderRadius: BorderRadius.circular(BaseStyles.borderRadius()));
+  }
+}
